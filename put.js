d6713195ffea33b1661c636a68b91b48f7d8760d
Kat Marchán <kzm@sykosomatic.org>
2016-11-18 19:12:34 -0800

    refactored put

--- put.js
+++ put.js
@@ -34,53 +34,30 @@ function putStream (cache, inputStream, opts, cb) {
     opts = null
   }
   opts = opts || {}
+  cb = dezalgo(cb)
   var startTime = +(new Date())
   var logger = wrapLogger(opts.logger || noop)
-  var tmpTarget = path.join(cache, 'tmp', (opts.prefix || '') + randomstring.generate())
+  var tmpTarget = path.join(cache, 'tmp', (opts.tmpPrefix || '') + randomstring.generate())
   mkdirp(tmpTarget, function (err) {
     if (err) { return cb(err) }
-    var hash = crypto.createHash(opts.hash || 'sha256')
-    var hashStream = through(function (chunk, enc, cb) {
-      hash.update(chunk, enc)
-      cb(null, chunk)
-    })
-    var outStream = tar.extract(tmpTarget)
-    pumpify(
-      inputStream,
-      hashStream,
-      zlib.Unzip(),
-      outStream
-    ).on('error', function () {
-      rimraf(tmpTarget, function (err) {
-        if (err) { cb(err) }
-      })
-    })
-    outStream.on('finish', moveToDestination)
-
-    function moveToDestination () {
-      logger('verbose', 'Temporary file written. Moving to main cache.')
-      var digest = hash.digest('hex')
-      var destination = contentPath(cache, digest)
-      mv(tmpTarget, destination, {
-        mkdirp: true, clobber: !!opts.clobber
-      }, function (err) {
-        if (err) {
-          if (err.code === 'EEXIST') {
-            logger('verbose', digest, 'already has an entry in the cache. Skipping move')
-          } else if (err.code === 'EBUSY') {
-            logger('verbose', digest, 'exists and is already being accessed. Skipping move.')
-          } else {
-            return cb(err)
-          }
-        }
-        rimraf(tmpTarget, function (err) {
+    pipeToTmp(inputStream, tmpTarget, opts, function (err, digest) {
+      if (err) { return cb(err) }
+      logger('verbose', 'Temporary file written. Verifying.')
+      var verifier = opts.verify || function (target, digest, cb) { cb() }
+      verifier(tmpTarget, digest, function (err) {
+        if (err) { return cb(err) }
+        logger('verbose', 'Verified. Moving to final cache destination')
+        moveToDestination(tmpTarget, cache, digest, logger, opts, function (err) {
           if (err) { return cb(err) }
-          var timeDiff = +(new Date()) - startTime
-          logger('verbose', 'processed', digest, 'in', timeDiff + 'ms')
-          cb(null, digest)
+          rimraf(tmpTarget, function (err) {
+            if (err) { return cb(err) }
+            var timeDiff = +(new Date()) - startTime
+            logger('verbose', 'processed', digest, 'in', timeDiff + 'ms')
+            cb(null, digest)
+          })
         })
       })
-    }
+    })
   })
 }
 
@@ -96,4 +73,92 @@ function wrapLogger (logObj) {
       logObj.apply(null, arguments)
     }
   }
+}
+
+function pipeToTmp (inputStream, tmpTarget, opts, cb) {
+  var hash = crypto.createHash(opts.hash || 'sha256')
+  var hashStream = through(function (chunk, enc, cb) {
+    hash.update(chunk, enc)
+    cb(null, chunk)
+  })
+
+  inputStream.on('data', function headerCheck (c) {
+    pumpify(
+      inputStream,
+      hashStream,
+      makeOutStream(c, tmpTarget, opts).on('finish', function () {
+        cb(null, hash.digest('hex'))
+      })
+    ).on('error', function () {
+      rimraf(tmpTarget, function (err) {
+        if (err) { cb(err) }
+      })
+    })
+
+    // remove and re-emit
+    inputStream.removeListener('data', headerCheck)
+    inputStream.emit('data', c)
+  })
+}
+
+function makeOutStream (c, target, opts) {
+  if (opts.filename) {
+    return fs.createWriteStream(path.join(target, opts.filename))
+  } else if (c[0] === 0x1F && c[1] === 0x8B && c[2] === 0x08) {
+    return pumpify(zlib.Unzip(), makeTarStream(target, opts))
+  } else if (hasTarHeader(c)) {
+    return makeTarStream(target, opts)
+  } else {
+    return fs.createWriteStream(path.join(target, 'index.js'))
+  }
+}
+
+function makeTarStream (target, opts) {
+  return tar.extract(target, {
+    map: function (header) {
+      if (process.platform !== 'win32') {
+        header.uid = opts.uid || header.uid
+        header.gid = opts.gid || header.gid
+      }
+      return header
+    },
+    ignore: opts.ignore,
+    dmode: opts.dmode,
+    fmode: opts.fmode,
+    strip: opts.strip
+  })
+}
+
+function hasTarHeader (c) {
+  return c[257] === 0x75 && // tar archives have 7573746172 at position
+         c[258] === 0x73 && // 257 and 003030 or 202000 at position 262
+         c[259] === 0x74 &&
+         c[260] === 0x61 &&
+         c[261] === 0x72 &&
+
+       ((c[262] === 0x00 &&
+         c[263] === 0x30 &&
+         c[264] === 0x30) ||
+
+        (c[262] === 0x20 &&
+         c[263] === 0x20 &&
+         c[264] === 0x00))
+}
+
+function moveToDestination (tmpTarget, cache, digest, logger, opts, cb) {
+  var destination = contentPath(cache, digest)
+  mv(tmpTarget, destination, {
+    mkdirp: true, clobber: !!opts.clobber
+  }, function (err) {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        logger('verbose', digest, 'already has an entry in the cache. Skipping move. Use the clobber option to force copy')
+      } else if (err.code === 'EBUSY') {
+        logger('verbose', digest, 'exists and is already being accessed. Skipping move.')
+      } else {
+        return cb(err)
+      }
+    }
+    cb()
+  })
 }
