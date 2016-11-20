980a9aa183b3cdbe80c55bdbe59935e598076240
Kat Marchán <kzm@sykosomatic.org>
2016-11-19 22:33:54 -0800

    index: find returns entry object

--- get.js
+++ get.js
@@ -3,16 +3,21 @@ var read = require('./lib/content/read')
 
 module.exports.directory = directory
 function directory (cache, key, destination, opts, cb) {
-  index.find(cache, key, function (err, digest) {
+  index.find(cache, key, function (err, data) {
     if (err) { return cb(err) }
-    read.asDirectory(cache, digest, destination, opts, cb)
+    read.asDirectory(cache, data.digest, destination, opts, cb)
   })
 }
 
 module.exports.tarball = tarball
 function tarball (cache, key, destination, opts, cb) {
-  index.find(cache, key, function (err, digest) {
+  index.find(cache, key, function (err, data) {
     if (err) { return cb(err) }
-    read.asTarball(cache, digest, destination, opts, cb)
+    read.asTarball(cache, data.digest, destination, opts, cb)
   })
+}
+
+module.exports.info = info
+function info (cache, key, cb) {
+  index.find(cache, key, cb)
 }
