a342d73f69334448519b0422bd11afdb51bd9898
Kat Marchán <kzm@sykosomatic.org>
2016-11-19 22:57:24 -0800

    get: nicer errors when not found

--- get.js
+++ get.js
@@ -5,6 +5,7 @@ module.exports.directory = directory
 function directory (cache, key, destination, opts, cb) {
   index.find(cache, key, function (err, data) {
     if (err) { return cb(err) }
+    if (!data) { return cb(notFoundError(cache, key)) }
     read.asDirectory(cache, data.digest, destination, opts, cb)
   })
 }
@@ -13,6 +14,7 @@ module.exports.tarball = tarball
 function tarball (cache, key, destination, opts, cb) {
   index.find(cache, key, function (err, data) {
     if (err) { return cb(err) }
+    if (!data) { return cb(notFoundError(cache, key)) }
     read.asTarball(cache, data.digest, destination, opts, cb)
   })
 }
@@ -20,4 +22,12 @@ function tarball (cache, key, destination, opts, cb) {
 module.exports.info = info
 function info (cache, key, cb) {
   index.find(cache, key, cb)
+}
+
+function notFoundError (cache, key) {
+  var err = new Error('content not found')
+  err.code = 'ENOENT'
+  err.cache = cache
+  err.key = key
+  return err
 }
