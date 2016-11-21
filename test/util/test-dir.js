d112f5b6a0dfdf70814d1bba42b9db705fa90174
Kat Marchán <kzm@sykosomatic.org>
2016-11-20 22:46:51 -0800

    test: correctly remove test/cache on tearDown

--- test/util/test-dir.js
+++ test/util/test-dir.js
@@ -10,21 +10,19 @@ function testDir (filename) {
   var dir = path.join(cacheDir, path.basename(filename, '.js'))
   mkdirp.sync(dir)
   process.chdir(dir)
-  if (!process.env.KEEPCACHE) { setupCleanup(dir) }
+  if (!process.env.KEEPCACHE) {
+    tap.tearDown(function () {
+      rimraf.sync(cacheDir)
+    })
+    tap.afterEach(function (cb) {
+      reset(dir)
+      cb()
+    })
+  }
   return dir
 }
 
 module.exports.reset = reset
 function reset (testDir) {
   rimraf.sync(testDir + '/*')
-}
-
-function setupCleanup (dir) {
-  tap.tearDown(function () {
-    rimraf.sync(dir)
-  })
-  tap.afterEach(function (cb) {
-    reset(dir)
-    cb()
-  })
 }
