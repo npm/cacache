721d420757f0bfcc4bc1a800c68a13a760762edd
Kat Marchán <kzm@sykosomatic.org>
2016-11-21 00:55:38 -0800

    put: protect the final move from unatomic moves

--- test/get.info.js
+++ test/get.info.js
@@ -117,3 +117,83 @@ test('get.info path-breaking characters', function (t) {
     t.end()
   })
 })
+
+test('get.info multiple index entries for key', function (t) {
+  var key = 'whatever'
+  var fixture = new Tacks(Dir({
+    'index': CacheIndex({
+      'whatever': [
+        { key: key, digest: 'deadbeef', time: 54321 },
+        { key: key, digest: 'bada55', time: 12345 }
+      ]
+    })
+  }))
+  fixture.create(CACHE)
+  get.info(CACHE, key, function (err, info) {
+    if (err) { throw err }
+    t.ok(info, 'cache hit')
+    t.equal(info.digest, 'bada55', 'most recent entry wins')
+    t.end()
+  })
+})
+
+test('get.info garbled data in index file', function (t) {
+  // Even though `index.insert()` is safe from direct
+  // race conditions, it's still possible for individual
+  // entries to become corrupted, or to be partially written,
+  // since `get.info` does not acquire a write-preventing lock.
+  //
+  // Because entries are newline-prepended and only one
+  // can be written at a time, the main possible corruption
+  // source is if an append fails mid-write (for example, due
+  // to the process crashing). In this case, the corrupt entry
+  // will simply be skipped.
+  var key = 'whatever'
+  var fixture = new Tacks(Dir({
+    'index': CacheIndex({
+      'whatever': '\n' + JSON.stringify({
+        key: key,
+        digest: 'deadbeef',
+        time: 54321
+      }) + '\n{"key": "' + key + '"\noway'
+    })
+  }))
+  fixture.create(CACHE)
+  get.info(CACHE, key, function (err, info) {
+    if (err) { throw err }
+    t.ok(info, 'cache hit in spite of crash-induced fail')
+    t.equal(info.digest, 'deadbeef', ' recent entry wins')
+    t.end()
+  })
+})
+
+test('get.info hash conflict in same bucket', function (t) {
+  // This... is very unlikely to happen. But hey.
+  var entry = {
+    key: 'whatever',
+    digest: 'deadbeef',
+    time: 12345,
+    metadata: 'yay'
+  }
+  var fixture = new Tacks(Dir({
+    'index': CacheIndex({
+      'whatever': [
+        { key: 'ohnoes', digest: 'welp!' },
+        entry,
+        { key: 'nope', digest: 'bada55' }
+      ]
+    })
+  }))
+  fixture.create(CACHE)
+  get.info(CACHE, entry.key, function (err, info) {
+    if (err) { throw err }
+    t.ok(info, 'cache hit')
+    delete info.path
+    t.deepEqual(
+      info,
+      entry,
+      'got the right one even though different keys exist in index'
+    )
+    t.end()
+  })
+})
