d74b7453e117396a29e308e44d0c524c681eeaeb
Kat Marchán <kzm@sykosomatic.org>
2016-11-21 23:39:52 -0800

    test: simplify put test

--- test/put.js
+++ test/put.js
@@ -1,11 +1,8 @@
 var crypto = require('crypto')
 var fs = require('fs')
-var mkdirp = require('mkdirp')
 var path = require('path')
 var test = require('tap').test
 var testDir = require('./util/test-dir')(__filename)
-var through = require('through2')
-var zlib = require('zlib')
 
 var CACHE = path.join(testDir, 'cache')
 var contentPath = require('../lib/content/path')
@@ -13,31 +10,26 @@ var contentPath = require('../lib/content/path')
 // because it's the function the other writers call.
 var put = require('../put')
 
-var CONTENT = 'foobarbaz'
 test('basic file put', function (t) {
-  fs.writeFile('index.js', CONTENT, function (err) {
+  var CONTENT = 'foobarbaz'
+  var DIGEST = crypto.createHash('sha256').update(CONTENT).digest('hex')
+  fs.writeFile('foo.txt', CONTENT, function (err) {
     if (err) { throw err }
     var key = 'whatever'
-    var hash = crypto.createHash('sha256')
-    var stream = fs.createReadStream(
-      'index.js'
-    ).pipe(through(function (chunk, enc, next) {
-      hash.update(chunk, enc)
-      next(null, chunk)
-    }))
-    put.stream(CACHE, key, stream, function (err) {
+    var stream = fs.createReadStream('foo.txt')
+    put.stream(CACHE, key, stream, function (err, foundDigest) {
       if (err) { throw err }
-      var digest = hash.digest('hex')
-      var content = contentPath(CACHE, digest)
-      t.plan(2)
-      fs.stat(content, function (err, stat) {
+      var cpath = contentPath(CACHE, foundDigest)
+      t.plan(3)
+      t.equal(foundDigest, DIGEST, 'returned digest matches expected')
+      fs.stat(cpath, function (err, stat) {
         if (err) { throw err }
         t.ok(
           stat.isFile(),
           'content inserted as a single file'
         )
       })
-      fs.readFile(content, 'utf8', function (err, data) {
+      fs.readFile(cpath, 'utf8', function (err, data) {
         if (err) { throw err }
         t.equal(
           data,
