0b9d0c1acde9f74f467d5771748599ec5f1bfa76
Kat Marchán <kzm@sykosomatic.org>
2017-01-29 16:51:07 +0100

    get: callback-based, bulk get functions

--- get.js
+++ get.js
@@ -1,11 +1,34 @@
 var index = require('./lib/entry-index')
+var finished = require('mississippi').finished
 var pipe = require('mississippi').pipe
 var read = require('./lib/content/read')
 var through = require('mississippi').through
 
-module.exports.stream = stream
+module.exports = function get (cache, key, opts, cb) {
+  return getData(false, cache, key, opts, cb)
+}
+module.exports.byDigest = function getByDigest (cache, digest, opts, cb) {
+  return getData(true, cache, digest, opts, cb)
+}
+function getData (byDigest, cache, key, opts, cb) {
+  if (!cb) {
+    cb = opts
+    opts = null
+  }
+  opts = opts || {}
+  var src = (byDigest ? getStream.byDigest : getStream)(cache, key, opts)
+  var data = ''
+  var meta
+  src.on('data', function (d) { data += d })
+  src.on('metadata', function (m) { meta = m })
+  finished(src, function (err) {
+    cb(err, data, meta)
+  })
+}
+
+module.exports.stream = getStream
 module.exports.stream.byDigest = read.readStream
-function stream (cache, key, opts) {
+function getStream (cache, key, opts) {
   var stream = through()
   index.find(cache, key, function (err, data) {
     if (err) { return stream.emit('error', err) }
