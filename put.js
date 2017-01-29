c60566c80db2eca10415b50b4d2ea2152c415d9d
Kat Marchán <kzm@sykosomatic.org>
2017-01-29 16:51:17 +0100

    put: callback-based bulk insert

--- put.js
+++ put.js
@@ -1,9 +1,31 @@
 var index = require('./lib/entry-index')
+var pipe = require('mississippi').pipe
 var putContent = require('./lib/content/put-stream')
+var through = require('mississippi').through
 var to = require('mississippi').to
+
+module.exports = putData
+function putData (cache, key, data, opts, cb) {
+  if (!cb) {
+    cb = opts
+    opts = null
+  }
+  opts = opts || {}
+  var src = through()
+  var digest
+  var dest = putStream(cache, key, opts)
+  dest.on('digest', function (d) { digest = d })
+  pipe(src, dest, function (err) {
+    cb(err, digest)
+  })
+  src.write(data, function () {
+    src.end()
+  })
+}
 
 module.exports.stream = putStream
 function putStream (cache, key, opts) {
+  opts = opts || {}
   var digest
   var contentStream = putContent(cache, opts).on('digest', function (d) {
     digest = d
