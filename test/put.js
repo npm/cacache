e4391c083f1d0290c46c734d116e292bdb4d03a5
Kat Marchán <kzm@sykosomatic.org>
2017-01-29 16:10:40 +0100

    test: stubbed out toplevel put tests

---
+++ test/put.js
@@ -0,0 +1,5 @@
+var test = require('tap').test
+
+test('stream ends when piped stream finishes')
+test('signals error if error writing to cache')
+test('adds correct entry to index before finishing')
