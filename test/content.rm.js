b9797c50e74e17a835c4c3a19e32127bc2027dc3
Kat Marchán <kzm@sykosomatic.org>
2017-02-24 21:24:03 -0800

    test(rm): port tests over to new api

--- test/content.rm.js
+++ test/content.rm.js
@@ -1,42 +1,45 @@
 'use strict'
 
-var fs = require('graceful-fs')
-var path = require('path')
-var Tacks = require('tacks')
-var test = require('tap').test
-var testDir = require('./util/test-dir')(__filename)
+const fs = require('graceful-fs')
+const path = require('path')
+const Promise = require('bluebird')
+const Tacks = require('tacks')
+const test = require('tap').test
+const testDir = require('./util/test-dir')(__filename)
 
-var CACHE = path.join(testDir, 'cache')
-var Dir = Tacks.Dir
-var File = Tacks.File
-var rm = require('../lib/content/rm')
+Promise.promisifyAll(fs)
+
+const CACHE = path.join(testDir, 'cache')
+const Dir = Tacks.Dir
+const File = Tacks.File
+const rm = require('../lib/content/rm')
 
 test('removes a content entry', function (t) {
-  var fixture = new Tacks(Dir({
+  const fixture = new Tacks(Dir({
     'content': Dir({
       'deadbeef': File('')
     })
   }))
   fixture.create(CACHE)
-  rm(CACHE, 'deadbeef', function (err) {
-    t.ifError(err, 'rm ran without error')
-    fs.stat(path.join(CACHE, 'content', 'deadbeef'), function (err) {
-      t.ok(err, 'fs.stat failed on rmed content')
-      t.equal('ENOENT', err.code, 'file does not exist anymore')
-      t.end()
-    })
+  return rm(CACHE, 'deadbeef').then(() => (
+    fs.statAsync(path.join(CACHE, 'content', 'deadbeef'))
+  )).then(() => {
+    throw new Error('expected an error')
+  }).catch(err => {
+    t.ok(err, 'fs.stat failed on rmed content')
+    t.equal('ENOENT', err.code, 'file does not exist anymore')
   })
 })
 
 test('works fine if entry missing', function (t) {
-  var fixture = new Tacks(Dir({}))
+  const fixture = new Tacks(Dir({}))
   fixture.create(CACHE)
-  rm(CACHE, 'deadbeef', function (err) {
-    t.ifError(err, 'rm ran without error')
-    fs.stat(path.join(CACHE, 'content', 'deadbeef'), function (err) {
-      t.ok(err, 'fs.stat failed on rmed content')
-      t.equal('ENOENT', err.code, 'file does not exist anymore')
-      t.end()
-    })
+  return rm(CACHE, 'deadbeef').then(() => (
+    fs.statAsync(path.join(CACHE, 'content', 'deadbeef'))
+  )).then(() => {
+    throw new Error('expected an error')
+  }).catch(err => {
+    t.ok(err, 'fs.stat failed on rmed content')
+    t.equal('ENOENT', err.code, 'file does not exist anymore')
   })
 })
