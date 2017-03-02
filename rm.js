4205cf0e9f6258c9e76a89e058ad22b9ce01fba4
Kat Marchán <kzm@sykosomatic.org>
2017-03-01 18:49:07 -0800

    feat(rm): added memoization support to rm

--- rm.js
+++ rm.js
@@ -2,21 +2,25 @@
 
 const Promise = require('bluebird')
 
-const rmContent = require('./lib/content/rm')
 const index = require('./lib/entry-index')
+const memo = require('./lib/memoization')
 const rimraf = Promise.promisify(require('rimraf'))
+const rmContent = require('./lib/content/rm')
 
 module.exports.all = all
 function all (cache) {
+  memo.clearMemoized()
   return rimraf(cache)
 }
 
 module.exports.entry = entry
 function entry (cache, key) {
+  memo.clearMemoized()
   return index.delete(cache, key)
 }
 
 module.exports.content = content
 function content (cache, address) {
+  memo.clearMemoized()
   return rmContent(cache, address)
 }
