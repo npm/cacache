fa336a4730727b53f2ef404e37917b2bff599b40
Kat Marchán <kzm@sykosomatic.org>
2017-03-02 14:39:43 -0800

    test(util): shiny new cache mocking utilities

---
+++ test/util/cache-content.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const contentPath = require('../../lib/content/path')
+const path = require('path')
+const Tacks = require('tacks')
+
+const Dir = Tacks.Dir
+const File = Tacks.File
+
+module.exports = CacheContent
+function CacheContent (entries, hashAlgorithm) {
+  hashAlgorithm = hashAlgorithm || 'sha512'
+  var tree = Dir({})
+  Object.keys(entries).forEach(function (k) {
+    const cpath = contentPath('', k, hashAlgorithm)
+    const content = entries[k]
+    const parts = cpath.split(path.sep)
+    insertContent(tree, parts, content)
+  })
+  return tree
+}
+
+function insertContent (tree, pathTo, content) {
+  const key = pathTo[0]
+  if (pathTo.length <= 1) {
+    tree.contents[key] = File(content)
+  } else {
+    tree.contents[key] = tree.contents[key] || Dir({})
+    insertContent(tree.contents[key], pathTo.slice(1), content)
+  }
+}
--- test/util/cache-index.js
+++ test/util/cache-index.js
@@ -1,23 +1,25 @@
 'use strict'
 
-var hashKey = require('../../lib/entry-index')._hashKey
-var Tacks = require('tacks')
+const bucketPath = require('../../lib/entry-index')._bucketPath
+const path = require('path')
+const Tacks = require('tacks')
 
-var Dir = Tacks.Dir
-var File = Tacks.File
+const Dir = Tacks.Dir
+const File = Tacks.File
 
 // Creates a simulated index using the chained lookup structure, from
 // an unhashed version of the index (basically `cacache.ls`).
 //
 // The returned object is for use with Tacks
 module.exports = CacheIndex
-function CacheIndex (entries) {
-  var index = {}
+function CacheIndex (entries, hashAlgorithm) {
+  hashAlgorithm = hashAlgorithm || 'sha512'
+  var tree = Dir({})
   Object.keys(entries).forEach(function (k) {
-    var lines = entries[k]
-    var hashed = hashKey(k)
-    var prefix = hashed.slice(0, 2)
-    var serialised
+    const bpath = bucketPath('', k, hashAlgorithm)
+    const parts = bpath.split(path.sep)
+    let lines = entries[k]
+    let serialised
     if (typeof lines === 'string') {
       serialised = lines
     } else {
@@ -26,17 +28,24 @@ function CacheIndex (entries) {
       }
       serialised = lines.map(JSON.stringify).join('\n')
     }
-    index[prefix] = index[prefix] || {}
-    index[prefix][hashed] = index[prefix][hashed]
-    ? [index[prefix][hashed], serialised].join('\n')
-    : serialised
+    insertContent(tree, parts, serialised)
   })
-  Object.keys(index).forEach(function (prefix) {
-    var files = {}
-    Object.keys(index[prefix]).forEach(key => {
-      files[key] = File(index[prefix][key])
-    })
-    index[prefix] = Dir(files)
-  })
-  return Dir(index)
+  return tree
+}
+
+function insertContent (tree, pathTo, content) {
+  const key = pathTo[0]
+  if (pathTo.length <= 1) {
+    if (tree.contents[key]) {
+      tree.contents[key] = File([
+        tree.contents[key].contents,
+        content
+      ].join('\n'))
+    } else {
+      tree.contents[key] = File(content)
+    }
+  } else {
+    tree.contents[key] = tree.contents[key] || Dir({})
+    insertContent(tree.contents[key], pathTo.slice(1), content)
+  }
 }
