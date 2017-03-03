954b1b33f2c04b497749ee20fb68dad464fe3767
Kat Marchán <kzm@sykosomatic.org>
2017-03-02 23:58:01 -0800

    feat(memo): export memoization api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   ls: require('./ls'),
   get: require('./get'),
+  memo: require('./lib/memoization'),
   put: require('./put'),
   rm: require('./rm'),
   verify: require('./verify'),
