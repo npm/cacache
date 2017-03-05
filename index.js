2f72d0a7213828231dfa4853ad4037509fffe1b3
Kat Marchán <kzm@sykosomatic.org>
2017-03-04 23:13:12 -0800

    fix(api): keep memo cache mostly-internal

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
   ls: require('./ls'),
   get: require('./get'),
-  memo: require('./lib/memoization'),
   put: require('./put'),
   rm: require('./rm'),
   verify: require('./verify'),
