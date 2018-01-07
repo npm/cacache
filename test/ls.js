347dc3652d43f7c599bd670c127a80f35fe59a71
Kat Marchán <kzm@sykosomatic.org>
2018-01-06 19:40:01 -0800

    fix(ls): deleted entries could cause a premature stream EOF

--- test/ls.js
+++ test/ls.js
@@ -3,14 +3,15 @@
 const BB = require('bluebird')
 
 const CacheIndex = require('./util/cache-index')
+const contentPath = require('../lib/content/path')
 const finished = BB.promisify(require('mississippi').finished)
+const index = require('../lib/entry-index.js')
 const path = require('path')
 const Tacks = require('tacks')
 const test = require('tap').test
 const testDir = require('./util/test-dir')(__filename)
 
 const CACHE = path.join(testDir, 'cache')
-const contentPath = require('../lib/content/path')
 const File = Tacks.File
 
 const ls = require('..').ls
@@ -109,5 +110,60 @@ test('ignores non-dir files', function (t) {
   return ls(CACHE).then(listing => {
     t.equal(Object.keys(listing).length, 1, 'only 1 item in listing')
     t.equal(listing.whatever.key, 'whatever', 'only the correct entry listed')
+  })
+})
+
+test('correctly ignores deleted entries', t => {
+  const contents = {
+    'whatever': {
+      key: 'whatever',
+      integrity: 'sha512-deadbeef',
+      time: 12345,
+      metadata: 'omgsometa',
+      size: 234234
+    },
+    'whatnot': {
+      key: 'whatnot',
+      integrity: 'sha512-bada55',
+      time: 54321,
+      metadata: null,
+      size: 425345345
+    },
+    'whatwhere': {
+      key: 'whatwhere',
+      integrity: 'sha512-bada55e5',
+      time: 54321,
+      metadata: null,
+      size: 425345345
+    }
+  }
+  const fixture = new Tacks(CacheIndex(contents))
+  contents.whatever.path =
+    contentPath(
+      CACHE, contents.whatever.integrity)
+  contents.whatnot.path =
+    contentPath(
+      CACHE, contents.whatnot.integrity)
+  contents.whatwhere.path =
+    contentPath(
+      CACHE, contents.whatwhere.integrity)
+  fixture.create(CACHE)
+  return index.delete(CACHE, 'whatnot')
+  .then(() => ls(CACHE))
+  .then(listing => t.deepEqual(listing, {
+    whatever: contents.whatever,
+    whatwhere: contents.whatwhere
+  }, 'index contents correct'))
+  .then(() => {
+    const listing = []
+    const stream = ls.stream(CACHE)
+    stream.on('data', entry => {
+      listing[entry.key] = entry
+    })
+    return finished(stream)
+    .then(() => t.deepEqual(listing, {
+      whatever: contents.whatever,
+      whatwhere: contents.whatwhere
+    }, 'ls is streamable'))
   })
 })
